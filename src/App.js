c77d7ad7146fcf3f0a5b9a3234a7606346ea7e97
Milan Patel <patelmj1@uci.edu>
2022-10-06 14:23:56 -0700

    authorization and auth complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  /*Info needed to authenticate with Spotify
+    -> Endpoint, Client ID, Redirect URI once authenticated, Response type, what scopes our app wants authorization for
+  */
+  const CLIENT_ID = "a95ddc783a0549ab9f1b71bcd94692db";
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+  const SCOPE =
+    "playlist-read-private playlist-modify-private playlist-modify-public user-read-private";
+
+  //state variable for the token and set it to an empty string by default.
+  const [token, setToken] = useState("");
+
+  /*
+  When App is opened (useEffect is called)
+    -> Check if we have a hash or if we have a token saved in local storage already
+      -> If we have a token stored, set our token state variable
+      -> If we don't, check if we have a hash
+        -> If so, perform tasks on that hash (which is a string) to extract the token
+
+  */
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Spotify React</h1>
+        {!token ? (
+          <a
+            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}
+          >
+            Login to Spotify
+          </a>
+        ) : (
+          <button onClick={logout}>Logout</button>
+        )}
       </header>
     </div>
   );
