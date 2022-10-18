fcf7c0e5c8abffaf717d2f68e12740f80bbac240
Milan Patel <patelmj1@uci.edu>
2022-10-18 13:50:45 -0700

    handled errors when token is expired by requiring reauth. changed login from <a> to <button> with help of onclick to redirect (to keep styles consistent).

--- src/components/App.js
+++ src/components/App.js
@@ -25,6 +25,8 @@ function App() {
     const hash = window.location.hash;
     let token = window.localStorage.getItem("token");
 
+    console.log(token);
+
     //if user is not already logged in, use the hash to extract the token
     if (!token && hash) {
       token = hash
@@ -38,28 +40,34 @@ function App() {
     }
 
     //if the user is logged in, use Axios to make GET requests to playlists and user endpoint
-    if (token) {
-      const getPlaylists = async () => {
-        const { data } = await axios.get(
-          "https://api.spotify.com/v1/me/playlists",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        setPlaylists(data.items); //update playlists state
-      };
-
-      const getUsername = async () => {
-        const { data } = await axios.get("https://api.spotify.com/v1/me", {
+    const getPlaylists = async () => {
+      await axios
+        .get("https://api.spotify.com/v1/me/playlists", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
+        })
+        .then((response) => setPlaylists(response.data.items))
+        .catch(function (error) {
+          //the token may be expired (causing axios 401 error) and the user may need to relogin.
+          console.log(error);
+          setToken(""); //reset token so that user is forced to login again.
         });
-        setUsername(data.display_name);
-      };
+    };
+
+    const getUsername = async () => {
+      const { data } = await axios.get("https://api.spotify.com/v1/me", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUsername(data.display_name);
+    };
+
+    if (token) {
       getPlaylists();
+    }
+    if (token) {
       getUsername();
     }
     setToken(token); //update token state so we know user is logged in
@@ -71,18 +79,20 @@ function App() {
     window.localStorage.removeItem("token");
   };
 
+  //when the user logs in, send them to Spotify to authenticate
+  const authenticate = () => {
+    window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+  };
+
   return (
     <div className="App">
       <div className="header">
         <h1>Friendlier</h1>
         <h2>An easy way to create clean versions of your Spotify playlists.</h2>
         {!token ? (
-          <a
-            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}
-            className="button"
-          >
+          <button onClick={authenticate} className="button">
             LOGIN TO SPOTIFY
-          </a>
+          </button>
         ) : (
           <button className="button" onClick={logout}>
             LOGOUT
